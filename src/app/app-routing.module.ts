059897fdaca088c24b02abd1a927268935128f4a
jgbarbosa2356 <jgbarbosac32@gmail.com>
2025-03-27 17:47:10 -0300

    page RegisterUser

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CadastroPage } from './cadaster/cadastro.page';
+
 const routes: Routes = [
   { path: '', redirectTo: 'cadastro', pathMatch: 'full' },
   { path: 'cadastro', component: CadastroPage },
-  { path: 'esqueci-senha', loadChildren: () => import('./Esqueceu-senha/esqueceu-senha.module').then(m => m.EsqueciSenhaPageModule) },  // Rota para a tela de "Esqueci a senha"
+  { path: 'esqueci-senha', loadChildren: () => import('./Esqueceu-senha/esqueceu-senha.module').then(m => m.EsqueciSenhaPageModule) },
+  {
+    path: 'registerUser', loadChildren: () => import('./RegisterUser/registerUser.module').then(m => m.RegisterUserModule)
+  }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
 })
 export class AppRoutingModule {}
